da29a7873e9504bff6887142ce8b83deafa36f41
DavidBrigand <159307967+DavidBrigand@users.noreply.github.com>
2025-03-13 14:05:23 +0100

    Upload des fichiers
    Version 1

---
+++ default.nix
@@ -0,0 +1,18 @@
+{
+  imports = [
+    ./custom.nix
+    ./cifs.nix
+    #./version.nix
+    #./nh.nix
+    ./boot.nix
+    ./gnome.nix
+    ./fstrim.nix
+    ./gaming.nix
+    #./firefox.nix
+    #./paquets.nix
+    ./pipewire.nix
+    ./nvidia.nix
+    ./imprimante.nix
+    ./system.nix
+  ];
+}
